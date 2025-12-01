bf6242612169bd913be6de3dc249101165cdcaa0
Dyashin_Sep_Kantharaj_BM <kantharajbhandary@example.com>
2025-12-01 23:30:41 +0530

    first commit

---
+++ sharathgs.py
@@ -0,0 +1,21 @@
+first_name = "sharath"
+lost_name = "gs"
+full_name = first_name + "  " + lost_name
+print(full_name)
+message ="this is a worning!"
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("worning","error"))
+
+name = '''sharath said "hello"
+           sharath said "hi"
+'''
+print(name)
+
+message = "worning!"
+print(message.upper()*10)
+name = "sharath"
+print(name[1:4:3])
+s = "sharath \tis good boy"
+print(s)
